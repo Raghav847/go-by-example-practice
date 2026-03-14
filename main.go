6abb8ef20ef2ed2ae9ec06b7a659478084efdbf6
Raghav Khandelwal <raaghav.khandelwal@gmail.com>
2026-03-14 15:01:09 -0700

    init

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+func main() {
+	//fmt.Println(HelloWorld())
+	//fmt.Println(Values())
+	//Variables()
+	Constants()
+}
---
+++ values.go
@@ -0,0 +1,5 @@
+package main
+
+func Values() (string, int, bool) {
+	return "go" + "lang", 1 + 1, true && false
+}
