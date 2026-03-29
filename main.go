7b10cf1696481512e02a449f6772a221797073a2
Raghav Khandelwal <raaghav.khandelwal@gmail.com>
2026-03-28 19:40:30 -0700

    rune

--- main.go
+++ main.go
@@ -1,18 +1,7 @@
 package main
 
-import "fmt"
-
 func main() {
 	//test every func here
-	i := 1
-	fmt.Println("initial:", i)
-
-	zeroval(i)
-	fmt.Println("zeroval:", i)
-
-	zeroptr(&i)
-	fmt.Println("zeroptr", i)
-
-	fmt.Println("pointer:", &i)
+	srune()
 
 }
