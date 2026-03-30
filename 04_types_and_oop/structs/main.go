9afa2492a2703e6f56bb1b4448abd22d30a7a4f3
Raghav Khandelwal <raaghav.khandelwal@gmail.com>
2026-03-29 23:03:27 -0700

    methods

--- 04_types_and_oop/structs/main.go
+++ 04_types_and_oop/structs/main.go
@@ -17,4 +17,22 @@ func main() {
 	fmt.Println(person{"Bob", 20})
 	fmt.Println(person{name: "Alice", age: 30})
 	fmt.Println(newPerson("Jon"))
+
+	s := person{name: "Sean", age: 50}
+	fmt.Println(s.name)
+
+	sp := &s
+	fmt.Println(sp.age)
+
+	sp.age = 51
+	fmt.Println(sp.age)
+
+	dog := struct {
+		name   string
+		isGood bool
+	}{
+		"Rex",
+		true,
+	}
+	fmt.Println(dog)
 }
