64b28f86e5d9ebe6ca0ca2c72fd62b6c83ee09ed
Raghav Khandelwal <raaghav.khandelwal@gmail.com>
2026-03-31 23:55:09 -0700

    enums

--- 04_types_and_oop/enums/main.go
+++ 04_types_and_oop/enums/main.go
@@ -2,6 +2,43 @@ package main
 
 import "fmt"
 
+type ServerState int
+
+const (
+	StateIdle ServerState = iota
+	StateConnected
+	StateError
+	StateRetrying
+)
+
+var stateName = map[ServerState]string{
+	StateIdle:      "idle",
+	StateConnected: "connected",
+	StateError:     "error",
+	StateRetrying:  "retrying",
+}
+
+func (ss ServerState) String() string {
+	return stateName[ss]
+}
+
 func main() {
-	fmt.Println("TODO: implement enums example")
+	ns := transition(StateIdle)
+	fmt.Println(ns)
+
+	ns2 := transition(ns)
+	fmt.Println(ns2)
+}
+
+func transition(s ServerState) ServerState {
+	switch s {
+	case StateIdle:
+		return StateConnected
+	case StateConnected, StateRetrying:
+		return StateIdle
+	case StateError:
+		return StateError
+	default:
+		panic(fmt.Errorf("unknown state: %s", s))
+	}
 }
