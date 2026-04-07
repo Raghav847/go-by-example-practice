2365f461af6bfa27cafbcc75d6a24c6f3ddb962f
Raghav Khandelwal <raaghav.khandelwal@gmail.com>
2026-04-06 21:54:21 -0700

    itors

--- 04_types_and_oop/range_over_iterators/main.go
+++ 04_types_and_oop/range_over_iterators/main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"iter"
+	"slices"
+)
+
+type List[T any] struct {
+	head, tail *element[T]
+}
+
+type element[T any] struct {
+	next *element[T]
+	val  T
+}
+
+func (lst *List[T]) Push(v T) {
+	if lst.tail == nil {
+		lst.head = &element[T]{val: v}
+		lst.tail = lst.head
+	} else {
+		lst.tail.next = &element[T]{val: v}
+		lst.tail = lst.tail.next
+	}
+}
+
+func (lst *List[T]) All() iter.Seq[T] {
+	return func(yield func(T) bool) {
+		for e := lst.head; e != nil; e = e.next {
+			if !yield(e.val) {
+				return
+			}
+		}
+	}
+}
 
 func main() {
-	fmt.Println("TODO: implement range over iterators example")
+	lst := List[int]{}
+	lst.Push(10)
+	lst.Push(13)
+	lst.Push(23)
+
+	for e := range lst.All() {
+		fmt.Println(e)
+	}
+
+	all := slices.Collect(lst.All())
+	fmt.Println("all:", all)
 }
