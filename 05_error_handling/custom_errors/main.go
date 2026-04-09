1ab57f38a130cb637e0aaace40750b835d602adb
Raghav Khandelwal <raaghav.khandelwal@gmail.com>
2026-04-08 18:39:49 -0700

    custom err

--- 05_error_handling/custom_errors/main.go
+++ 05_error_handling/custom_errors/main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
+
+type argError struct {
+	arg     int
+	message string
+}
+
+func (e *argError) Error() string {
+	return fmt.Sprintf("%d - %s", e.arg, e.message)
+}
+
+func f(arg int) (int, error) {
+	if arg == 42 {
+		return -1, &argError{arg, "can't work with it"}
+	}
+	return arg + 3, nil
+}
 
 func main() {
-	fmt.Println("TODO: implement custom errors example")
+	_, err := f(42)
+	if ae, ok := errors.AsType[*argError](err); ok {
+		fmt.Println(ae.arg)
+	} else {
+		fmt.Println("err not match")
+	}
 }
