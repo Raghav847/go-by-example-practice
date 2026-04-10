ef17e5a08bd50c96af0ebca0fbbdcf1c0ccb9321
Raghav Khandelwal <raaghav.khandelwal@gmail.com>
2026-04-10 00:20:19 -0700

    goroutines

--- 06_concurrency/goroutines/main.go
+++ 06_concurrency/goroutines/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+func f(from string) {
+	for i := range 3 {
+		fmt.Println(from, ":", i)
+	}
+}
 
 func main() {
-	fmt.Println("TODO: implement goroutines example")
+	f("direct")
+
+	go f("goroutine")
+
+	go func(msg string) {
+		fmt.Println(msg)
+	}("going")
+
+	time.Sleep(time.Second)
+	fmt.Println("done")
 }
