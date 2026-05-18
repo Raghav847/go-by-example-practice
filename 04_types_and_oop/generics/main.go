0bb9392655747971b84df3a339cd7d0659f07d6a
Raghav Khandelwal <raaghav.khandelwal@gmail.com>
2026-05-17 17:20:48 -0700

    generics

--- 04_types_and_oop/generics/main.go
+++ 04_types_and_oop/generics/main.go
@@ -2,62 +2,40 @@ package main
 
 import "fmt"
 
-type Number interface {
-	int64 | float64
+func Map[T1, T2 any](s []T1, f func(T1) T2) []T2 {
+	r := make([]T2, len(s))
+	for i, v := range s {
+		r[i] = f(v)
+	}
+	return r
+}
+
+func Reduce[T1, T2 any](s []T1, initializer T2, f func(T2, T1) T2) T2 {
+	r := initializer
+	for _, v := range s {
+		r = f(r, v)
+	}
+	return r
+}
+
+func Filter[T any](s []T, f func(T) bool) []T {
+	var r []T
+	for _, v := range s {
+		if f(v) {
+			r = append(r, v)
+		}
+	}
+	return r
 }
 
 func main() {
-	ints := map[string]int64{
-		"first":  34,
-		"second": 12,
-	}
-
-	floats := map[string]float64{
-		"first":  35.98,
-		"second": 36.99,
-	}
-
-	fmt.Printf("Non-Generic Sums: %v and %v\n",
-		SumInts(ints),
-		SumFloats(floats))
-
-	fmt.Printf("Generic Sums: %v and %v\n",
-		SumIntsOrFloats(ints),
-		SumIntsOrFloats(floats))
-
-	fmt.Printf("Generic Sums with Constraint: %v and %v\n",
-		SumNumbers(ints),
-		SumNumbers(floats))
-}
-
-func SumInts(m map[string]int64) int64 {
-	var s int64
-	for _, v := range m {
-		s += v
-	}
-	return s
-}
-
-func SumFloats(m map[string]float64) float64 {
-	var s float64
-	for _, v := range m {
-		s += v
-	}
-	return s
-}
-
-func SumIntsOrFloats[K comparable, V int64 | float64](m map[K]V) V {
-	var s V
-	for _, v := range m {
-		s += v
-	}
-	return s
-}
-
-func SumNumbers[K comparable, V Number](m map[K]V) V {
-	var s V
-	for _, v := range m {
-		s += v
-	}
-	return s
+	words := []string{"Anakin", "Padme", "Luke", "Leia"}
+	filtered := Filter(words, func(s string) bool {
+		return s != "Luke"
+	})
+	fmt.Println(filtered)
+	lengths := Map(filtered, func(s string) int {
+		return len(s)
+	})
+	fmt.Println(lengths)
 }
