c13941632ea6ae332409e6b502a91f57520e3212
Raghav Khandelwal <raaghav.khandelwal@gmail.com>
2026-07-23 14:09:34 -0700

    new serve

---
+++ experiments/new_serve/api.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+type APIServer struct {
+	addr string
+}
+
+func NewAPIServer(addr string) *APIServer {
+	return &APIServer{
+		addr: addr,
+	}
+}
+
+func (s *APIServer) Run() error {
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /users/{userID}", func(w http.ResponseWriter, r *http.Request) {
+		userID := r.PathValue("userID")
+		w.Write([]byte("User ID: " + userID))
+	})
+
+	server := http.Server{
+		Addr:    s.addr,
+		Handler: RequestLoggerMiddleware(router),
+	}
+
+	log.Printf("Server has started %s", s.addr)
+
+	return server.ListenAndServe()
+}
+
+func RequestLoggerMiddleware(next http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("method: %s, path: %s", r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	}
+}
